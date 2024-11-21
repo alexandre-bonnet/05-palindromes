2d21d8879a632a83dd0ea43b4e839e818b7a0aa0
alexandre-bonnet <alexandre.bonnet@edu.esiee.fr>
2024-11-21 09:22:35 +0000

    Exercice 05 AlexandreBonnet

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
-#### Fonction secondaire
-
-
-def ispalindrome(p):
-
-    # votre code ici
-    
-    return False
+'''
+this method returns 'true' or 'false' 
+depending on if the string in parameter is a palindrome
+'''
+def ispalindrome(s):
+    '''
+    this method returns 'true' or 'false' 
+    depending on if the string in parameter is a palindrome
+    '''
+    s = s.lower()
+    accents = str.maketrans('àéèëêçûô','aeeeecuo')
+    s = s.translate(accents)
+    sprime = ""
+    for i in range(len(s)):
+        if (s[i]>='a' and s[i]<='z')or(s[i]>='0' and s[i]<='9'):
+            sprime += s[i]
+    print(sprime)
+    for i in range (int(len(sprime)/2)+1):
+        if sprime[i] != sprime[-(i+1)]:
+            return False
+    return True
 
 #### Fonction principale
 
 
 def main():
+    '''
+    this method returns is calling the palindrome() one
+    '''
 
-    # vos appels à la fonction secondaire ici
-
-    for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
+    for s in ["radar", "kayak", "level", "rotor", "civique",]:
         print(s, ispalindrome(s))
 
 
